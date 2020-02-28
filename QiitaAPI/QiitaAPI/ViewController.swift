44b14a4ef945a4d422e0de27b32f02f9cedd4936
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-02-28 20:19:17 +0900

    add code

--- QiitaAPI/QiitaAPI/ViewController.swift
+++ QiitaAPI/QiitaAPI/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    var articles = [[String: AnyObject]]()
+    
+    func getArticleData() {
+                
+        let baseURl = "https://qiita.com/api/v2/items"
+        
+        AF.request(baseURl, method: .get)
+            .responseJSON{ response in
+                switch response.result {
+                case .success(let value):
+                    print("Success! Got the data")
+                    let articles : JSON = JSON(value)
+                        if let article = articles.arrayObject {
+                            self.articles = article as! [[String: AnyObject]]
+                            print(articles)
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getArticleData()
     }
 
 
