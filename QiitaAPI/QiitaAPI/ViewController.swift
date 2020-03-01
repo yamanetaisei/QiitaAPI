78df85f8630d723a5370d51ad0905ecae7d8285e
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-02 00:56:23 +0900

    add code

--- QiitaAPI/QiitaAPI/ViewController.swift
+++ QiitaAPI/QiitaAPI/ViewController.swift
@@ -37,7 +37,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        getArticleData()
     }
 
 
