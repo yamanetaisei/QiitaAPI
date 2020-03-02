6223dfff71ad5f8bbd9533a901a2f1ab1d2f2f77
山根大生 <taisei@yamanedaiseinoMacBook-Pro.local>
2020-03-02 16:50:23 +0900

    add swiftfile, import Webkit

---
+++ QiitaAPI/QiitaAPI/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  QiitaAPI
+//
+//  Created by 山根大生 on 2020/03/02.
+//  Copyright © 2020 taisei. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewControler: UIViewController{
+    
+}
